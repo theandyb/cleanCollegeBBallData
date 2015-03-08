2c4e7bd5581f790e48a4a471c8ac417be39302da
Andy Beck <beckandy@umich.edu>
2015-03-08 12:08:07 -0400

    Added reclassify option

--- Makefile
+++ Makefile
@@ -2,8 +2,13 @@ data/final.csv: raw_data/all_data2.csv
 	Rscript src/mergeData.R
 
 raw_data/all_data2.csv:
+ifeq ($(RECLASS),1)
+	sed '/"Rank"/,/NCAA/!d' ./raw_data/all_stat.csv | sed '/script/,/\/script/d' > ./raw_data/all_stat2.csv
+	python ./src/allStat.py -r
+else
 	sed '/"Rank"/,/Reclassifying/!d' ./raw_data/all_stat.csv > ./raw_data/all_stat2.csv
 	python ./src/allStat.py
+endif
 
 clean:
 	rm raw_data/all_stat2.csv
--- src/allStat.py
+++ src/allStat.py
@@ -1,3 +1,16 @@
+import sys,getopt
+
+try:
+        opts,args = getopt.getopt(sys.argv[1:], "r",["recursive"])
+except getopt.GetoptError:
+        print "Lulz"
+
+reclass = False
+for o,a in opts:
+        if o == "-r":
+                reclass = True
+
+
 fName = "./raw_data/all_stat2.csv"
 num = 1
 outFile = "./raw_data/" + str(num) + ".csv"
@@ -5,11 +18,22 @@ out = open(outFile,'w')
 
 with open(fName,"r") as f:
 	for line in f:
-		if not "Reclassifying" in line:
-			out.write(line)
+		if reclass:
+			if not "Reclassifying" in line: 
+				if not "NCAA" in line:
+					out.write(line)
+				else:	
+					out.close()
+					num += 1
+					outFile = "./raw_data/" + str(num) + ".csv"
+					out = open(outFile,'w')
 		else:
-			out.close()
-			num += 1
-			outFile = "./raw_data/" + str(num) + ".csv"
-			out = open(outFile,'w')
+			if not "Reclassifying" in line:
+				out.write(line)
+			else:
+				out.close()
+				num += 1
+				outFile = "./raw_data/" + str(num) + ".csv"
+				out = open(outFile,'w')
+out.close()
 print("Done!")
